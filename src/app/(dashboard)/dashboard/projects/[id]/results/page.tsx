01284104ec4afbffe09aa6628691adda4b2a31a8
AkashMadbor <akashabrar@softvence.com>
2026-05-05 11:14:31 +0600

    fix: revert dashboard to old structure and separate alif's sub-user code

--- src/app/(dashboard)/dashboard/projects/[id]/results/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from "react";
-import AnalysisResults from "@/features/projects/components/AnalysisResults";
-
-export default function Page() {
-  return <AnalysisResults />;
-}
