1e2d40dd95b8a9f96584a10ad3c2cf79aa483bc7
joyeta   mondal <joyetamondal@bdCallings-Mac-mini-65.local>
2026-06-29 09:01:24 +0600

    integrated notification aPIs in the admin dashboard

---
+++ src/store/api/admin/Dashboard/Notifications/markRead.ts
@@ -0,0 +1,43 @@
+import { baseApi } from "@/store/api/baseApi";
+
+export const markReadApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    markNotificationRead: builder.mutation<
+      {
+        success: boolean;
+        message: string;
+        data: unknown;
+      },
+      string
+    >({
+      query: (id) => ({
+        url: `notifications/mark-read/${id}`,
+        method: "PATCH",
+      }),
+      invalidatesTags: ["Notifications"],
+    }),
+
+    markAllNotificationsRead: builder.mutation<
+      {
+        success: boolean;
+        message: string;
+        data: {
+          count: number;
+          message: string;
+        };
+      },
+      void
+    >({
+      query: () => ({
+        url: "notifications/mark-all-read",
+        method: "PATCH",
+      }),
+      invalidatesTags: ["Notifications"],
+    }),
+  }),
+});
+
+export const {
+  useMarkNotificationReadMutation,
+  useMarkAllNotificationsReadMutation,
+} = markReadApi;
